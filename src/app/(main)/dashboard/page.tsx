e662d289960c9acc88120131e2a1b112f8753eed
felix <54502652+TheVideoFelix@users.noreply.github.com>
2025-07-02 23:04:35 +0200

    feat(dashboard): implement activity addition form with transport options and buttons

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import {Container} from "@/components/ui";
 import {ChartData} from "chart.js";
 import FootprintOverTimeChart from "@/components/chart/footprintOverTimeChart";
 import TotalCarbonFootprintChart from "@/components/chart/totalCarbonFootprintChart";
+import Button from "@/components/ui/button";
 
 export default function Dashboard() {
 
@@ -40,12 +41,91 @@ export default function Dashboard() {
         ],
     };
 
+    const today = new Date().toISOString().split('T')[0];
 
 
     return (
         <>
-            <header className="flex flex-col justify-end h-[200px] bg-lime-950 text-amber-50 rounded-b-2xl">
 
+
+
+            <div className="fixed inset-0 z-40 bg-gray-900/10 backdrop-blur-sm" />
+
+            <div className="fixed inset-0 z-50 flex items-center justify-center">
+                <div className="bg-amber-50 p-8 rounded-lg shadow-sm w-1/3 h-[800px]">
+                    <Container maxWidth={600} className="w-full h-full">
+                        <h1 className="text-2xl font-bold">Add Activity</h1>
+                        <div className="mt-5 px-7">
+                            <ul className="flex justify-between">
+                                {[
+                                    { label: "Transport", active: true },
+                                    { label: "Energy", active: false },
+                                    { label: "Food", active: false },
+                                    { label: "Waste", active: false }
+                                ].map((item, index) => (
+                                    <Button
+                                        variant="text"
+                                        key={index}
+                                        active={item.active}
+                                    >
+                                        {item.label}
+                                    </Button>
+                                ))
+                                }
+                            </ul>
+                        </div>
+                        <div className="w-full h-px bg-gray-300 my-4"/>
+
+                        <form action="" className="flex flex-col justify-between ">
+                            <div className="flex flex-col gap-10">
+                                <div>
+                                    <label htmlFor="transport-type" className="block text-lime-900 font-semibold mb-2">Type</label>
+                                    <select
+                                        id="transport-type"
+                                        className="w-full p-3 rounded-lg border border-lime-900/50 bg-transparent focus:outline-none focus:ring-2 focus:ring-lime-900"
+                                    >
+                                        <option>Car (Gasoline)</option>
+                                        <option>Bus</option>
+                                        <option>Train</option>
+                                        <option>Bicycle</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label htmlFor="distance" className="block text-lime-900 font-semibold mb-2">Distance (km)</label>
+                                    <input
+                                        type="number"
+                                        id="distance"
+                                        placeholder="e.g., 25"
+                                        className="w-full p-3 rounded-lg border border-lime-900/50 bg-transparent focus:outline-none focus:ring-2 focus:ring-lime-900"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="date" className="block text-lime-900 font-semibold mb-2">Date</label>
+                                    <input
+                                        type="date"
+                                        id="date"
+                                        defaultValue={today}
+                                        className="w-full p-3 rounded-lg border border-lime-900/50 bg-transparent focus:outline-none focus:ring-2 focus:ring-lime-900"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-full flex gap-4 mt-8">
+                                <Button variant='box' size="full">
+                                    Cancel
+                                </Button>
+                                <Button variant='box' size="full" active={true}>
+                                    Add
+                                </Button>
+                            </div>
+                        </form>
+                    </Container>
+                </div>
+            </div>
+
+
+
+
+            <header className="flex flex-col justify-end h-[200px] bg-lime-950 text-amber-50 rounded-b-2xl">
                 <Container>
                     <div className="flex flex-col max-h-lg pb-10">
                         <h1 className="text-4xl font-bold py-3">
@@ -60,16 +140,27 @@ export default function Dashboard() {
 
 
             <section className="py-20">
-                <Container className="sm:p-5">
+                <Container className="sm:p-5" maxWidth={1400}>
 
 
-                    <div className="sm:rounded-2xl sm:px-5 sm:py-7 flex flex-col items-start sm:border-1 ">
+                    <div className="sm:rounded-2xl px-5 sm:py-7 flex flex-col items-start sm:border-1 ">
 
-                        <div className="">
-                            <h1 className="text-2xl">Total Carbon Footprint (June 2025) </h1>
-                            <div className="flex items-baseline gap-2 py-2">
-                                <span className="text-4xl font-bold text-lime-950 ">335kg CO₂e</span>
-                                <span className="text-orange-200">+12% vs last month</span>
+                        <div className="flex justify-between w-full items-center">
+                            <div className="">
+                                <h1 className="text-2xl">Total Carbon Footprint (June 2025) </h1>
+                                <div className="flex items-baseline gap-2 py-2">
+                                    <span className="text-4xl font-bold text-lime-950 ">335kg CO₂e</span>
+                                    <span className="text-orange-200">+12% vs last month</span>
+                                </div>
+                            </div>
+                            <div className="">
+                                <button
+                                    className="border-1 rounded-lg h-16 w-16 flex items-center justify-center
+                                    border-lime-900 bg-lime-900 text-amber-50 text-3xl hover:bg-transparent
+                                    hover:text-lime-900 transition-all duration-200 cursor-pointer"
+                                >
+                                    +
+                                </button>
                             </div>
                         </div>
 
@@ -142,7 +233,9 @@ export default function Dashboard() {
                     </div>
 
 
-                </Container>
+                </Container>                                    className="border-1 rounded-lg h-16 w-16 flex items-center justify-center
+                                    border-lime-900 bg-lime-900 text-amber-50 text-3xl hover:bg-transparent
+                                    hover:text-lime-900 transition-all duration-200 "
             </section>
         </>
     )
