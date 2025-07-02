4bb04e611f022775b7fde15508be7f91d3d50f10
felix <54502652+TheVideoFelix@users.noreply.github.com>
2025-07-02 23:03:53 +0200

    feat(container): add maxWidth prop to container

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -5,11 +5,16 @@ const containerStyles = tv({
     base: 'max-w-[1100px] w-11/12 mx-auto'
 });
 
-interface ContainerProps extends React.ComponentProps<'div'> {}
+interface ContainerProps extends React.ComponentProps<'div'> {
+    maxWidth?: number | string;
+}
 
-const Container = ({ className, children, ...props } : ContainerProps) => {
+const Container = ({ className, children, maxWidth, style, ...props } : ContainerProps) => {
     return (
-        <div className={containerStyles({ className })} {...props}>
+        <div
+            className={containerStyles({ className })}
+            style={{maxWidth, ...style}}
+            {...props}>
             {children}
         </div>
     );
